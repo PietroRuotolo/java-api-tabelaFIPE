134f8abca4f4cc8b33c2779a2d360841f389aa0c
Pietro Ruotolo <pietroschimidt03082007@gmail.com>
2026-05-18 14:05:12 -0300

    fix: Evita o programa crashar caso o usuário insira argumento inválido

--- src/main/java/fiap/com/fipeapi/main/Main.java
+++ src/main/java/fiap/com/fipeapi/main/Main.java
@@ -4,6 +4,7 @@ import fiap.com.fipeapi.model.Brands;
 import fiap.com.fipeapi.service.ApiConsume;
 import fiap.com.fipeapi.service.DataConverter;
 import fiap.com.fipeapi.service.FipeService;
+import fiap.com.fipeapi.service.TypeGetter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,11 +14,16 @@ import java.util.Scanner;
 public class Main {
 
     private Scanner scanner = new Scanner(System.in);
-    private final String ADDRESS = "https://parallelum.com.br/fipe/api/v1/%s/marcas";
+    private static final String BASE_URL = "https://parallelum.com.br/fipe/api/v1";
+    private static final String ADDRESS_TYPE = BASE_URL + "/%s/marcas";
+    private static final String ADDRESS_BRAND = BASE_URL + "/%s/marcas/%s/modelos";
+    private static final String ADDRESS_YEAR = BASE_URL + "/%s/marcas/%s/modelos/%s/anos/%s/";
     private final FipeService service;
     private ApiConsume apiConsume = new ApiConsume();
     private DataConverter converter = new DataConverter();
-    ///marcas/%s/modelos/%s/anos/%s/";
+
+    private TypeGetter typeGetter = new TypeGetter();
+
 
     public Main(FipeService service){
         this.service = service;
@@ -30,18 +36,19 @@ public class Main {
                 "\nCarros" +
                 "\nMotos" +
                 "\nCaminhões");
-        String type = scanner.nextLine().toLowerCase();
-        if(type.contains("car")){
-            type = "carros";
-        }else if(type.contains("mo")){
-            type = "motos";
-        }else{
-            type = "caminhoes";
+
+        String vehicleType = "";
+        while (vehicleType == ""){
+            vehicleType = typeGetter.getType(scanner.nextLine().toLowerCase());
         }
 
-        String json = apiConsume.getData(ADDRESS.formatted(type));
-        List<Brands> brands = converter.convert(json, Brands.class);
-
+        String json1 = apiConsume.getData(ADDRESS_TYPE.formatted(vehicleType));
+        List<Brands> brands = converter.convert(json1, Brands.class);
         System.out.println(brands);
+
+        System.out.print("\nSelecione a marca do carro [INSIRA O CÓDIGO]: ");
+        String brandInput = scanner.nextLine().toLowerCase();
+
+//        String json2 = apiConsume.getData(ADDRESS.formatted())
     }
 }
